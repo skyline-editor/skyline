e18cfd0b4726e575a55fb3adeb99b84f9afa45a0
firecraftgaming <eliyah@sundstroem.com>
2021-07-27 14:28:11 +0200

    feat: updated tokenizer to be 3x times as fast

--- components/code/codeEditor.tsx
+++ components/code/codeEditor.tsx
@@ -1,20 +1,88 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from '../../styles/CodeEditor.module.css'
-import { codeFromTokens, tokenize } from "./tokenizer";
+import { highlight } from "./tokenizer";
 
 export interface CodeEditorProps {
   initialValue: string;
   onChange?: (code: string) => void;
 }
 
+interface Cursor {
+  line: number;
+  column: number;
+}
+
+const Char = {
+  width: 11,
+  height: 20,
+};
+
+const state = { cursor: null };
+
 export const CodeEditor = ({
   initialValue,
 }) => {
   const [code, setCode] = useState(initialValue);
+  const [cursor, setCursor] = useState({
+    line: 7,
+    column: 1,
+  });
+
+  state.cursor = cursor;
+
+  useEffect(() => {
+    window.addEventListener('keydown', e => {
+      console.log(e.key, state.cursor);
+      if (e.key === 'ArrowDown') {
+        setCursor({
+          line: state.cursor.line + 1,
+          column: state.cursor.column,
+        });
+      }
+      if (e.key === 'ArrowUp') {
+        setCursor({
+          line: state.cursor.line - 1,
+          column: state.cursor.column,
+        });
+      }
+      if (e.key === 'ArrowLeft') {
+        setCursor({
+          line: state.cursor.line,
+          column: state.cursor.column - 1,
+        });
+      }
+      if (e.key === 'ArrowRight') {
+        setCursor({
+          line: state.cursor.line,
+          column: state.cursor.column + 1,
+        });
+      }
+    });
+  }, []);
   
   return (
-    <div className={styles.codeEditor}>
-      <div className={styles.line}>{codeFromTokens(tokenize(code))}</div>
+    <div
+      className={styles.codeEditor}
+    >
+      <Code code={code} />
+      <Cursors cursor={cursor} />
     </div>
   );
 }
+
+const Code = ({ code }: {code: string}) => {
+  return (
+    <div>
+      {highlight(code).map(v => (<div className={styles.line}>{v}</div>))}
+    </div>
+  );
+};
+const Cursors = ({ cursor }: {cursor: Cursor}) => {
+  return (
+    <div>
+      <div className={styles.cursor} style={{ top: Char.height * cursor.line, left: Char.width * cursor.column }} />
+    </div>
+  );
+};
+
+export default CodeEditor;
--- components/code/tokenizer.tsx
+++ components/code/tokenizer.tsx
@@ -1,24 +1,26 @@
 export interface ValueToken {
   type: 'string' | 'number' | 'boolean' | 'keyword' | 'variable' | 'comment' | 'static' | 'operator';
   value: string;
+  pos?: number;
 }
 export interface ArrayToken {
   type: 'parentheses' | 'brackets' | 'braces';
   value: Token[];
+  pos?: number;
 }
 
-const STRING = (value: string) => ({ type: 'string', value }) as ValueToken;
-const NUMBER = (value: string) => ({ type: 'number', value }) as ValueToken;
-const BOOLEAN = (value: string) => ({ type: 'boolean', value }) as ValueToken;
-const KEYWORD = (value: string) => ({ type: 'keyword', value }) as ValueToken;
-const VARIABLE = (value: string) => ({ type: 'variable', value }) as ValueToken;
-const COMMENT = (value: string) => ({ type: 'comment', value }) as ValueToken;
-const STATIC = (value: string) => ({ type: 'static', value }) as ValueToken;
-const OPERATOR = (value: string) => ({ type: 'operator', value }) as ValueToken;
+const STRING = (value: string, pos: number) => ({ type: 'string', value, pos }) as ValueToken;
+const NUMBER = (value: string, pos: number) => ({ type: 'number', value, pos }) as ValueToken;
+const BOOLEAN = (value: string, pos: number) => ({ type: 'boolean', value, pos }) as ValueToken;
+const KEYWORD = (value: string, pos: number) => ({ type: 'keyword', value, pos }) as ValueToken;
+const VARIABLE = (value: string, pos: number) => ({ type: 'variable', value, pos }) as ValueToken;
+const COMMENT = (value: string, pos: number) => ({ type: 'comment', value, pos }) as ValueToken;
+const STATIC = (value: string, pos: number) => ({ type: 'static', value, pos }) as ValueToken;
+const OPERATOR = (value: string, pos: number) => ({ type: 'operator', value, pos }) as ValueToken;
 
-const PARENTHESES = (value: Token[]) => ({ type: 'parentheses', value }) as ArrayToken;
-const BRACKETS = (value: Token[]) => ({ type: 'brackets', value }) as ArrayToken;
-const BRACES = (value: Token[]) => ({ type: 'braces', value }) as ArrayToken;
+const PARENTHESES = (value: Token[], pos: number) => ({ type: 'parentheses', value, pos }) as ArrayToken;
+const BRACKETS = (value: Token[], pos: number) => ({ type: 'brackets', value, pos }) as ArrayToken;
+const BRACES = (value: Token[], pos: number) => ({ type: 'braces', value, pos }) as ArrayToken;
 
 export type Token = ValueToken | ArrayToken | string;
 
@@ -30,64 +32,77 @@ const colors = {
   static: '#9A92FF',
   keyword: '#79CFFF',
   variable: '#83ADFF',
-  comment: '#57778F',
+  propery: '#83ADFF',
+  comment: '#46667E',
 };
 
 let list_id = 0;
 
-export function codeFromTokens(tokens: Token[]) {
-  return tokens.map(token => {
-    if (typeof token === 'string') return token;
-    if (token.type in colors) return (<span style={{ color: colors[token.type] }} key={list_id++} >{token.value}</span>);
-
-    if (typeof token.value !== 'string') {
-      const code = codeFromTokens(token.value);
-      if (token.type === 'parentheses') return ['(', code, ')'];
-      if (token.type === 'brackets') return ['{', code, '}'];
-      if (token.type === 'braces') return ['[', code, ']'];
-    }
-    return token.value;
-  });
+export function highlight(code: string) {
+  console.time('highlight');
+  console.time('tokenize');
+  const tokens = tokenize(code);
+  console.timeEnd('tokenize');
+  
+  console.time('code');
+  const html = codeFromTokens(tokens);
+  console.timeEnd('code');
+  
+  console.timeEnd('highlight');
+  return html;
 }
 
+function getElemsFromTokens(tokens: Token[]) {
+  const elems = [];
+  for (const token of tokens) {
+    if (typeof token === 'string') {
+      elems.push(token);
+      continue;
+    }
+    if (token.type in colors && typeof token.value === 'string') {
+      const lines = token.value.split('\n');
+      const result = [];
+      for (let i = 0; i < lines.length; i++) {
+        if (i > 0) result.push('\n');
+        if (token.type == 'comment') {
+          result.push(<span style={{ color: colors[token.type], fontStyle: 'italic' }} key={list_id++} >{lines[i]}</span>);
+          continue;
+        }
+        result.push(<span style={{ color: colors[token.type] }} key={list_id++} >{lines[i]}</span>);
+      }
+      
+      elems.push(...result);
+      continue;
+    }
 
-const breaking_chars = [
-  '.',
-  ':',
-  ';',
-  '{',
-  '}',
-  '(',
-  ')',
-  '[',
-  ']',
-  '<',
-  '>',
-  '+',
-  '-',
-  '*',
-  '/',
-  '%',
-  '&',
-  '|',
-  '^',
-  '=',
-  '!',
-  '~',
-  '?',
-  '*',
+    if (typeof token.value !== 'string') {
+      const code = getElemsFromTokens(token.value);
+      if (token.type === 'parentheses') elems.push('(', ...code, ')');
+      if (token.type === 'brackets') elems.push('{', ...code, '}');
+      if (token.type === 'braces') elems.push('[', ...code, ']');
+      continue;
+    }
+    elems.push(token.value);
+  }
+  return elems;
+}
+export function codeFromTokens(tokens: Token[]) {
+  const elems = getElemsFromTokens(tokens);
+  if (elems[elems.length - 1] !== '\n') elems.push('\n');
 
-  //strings
-  '\\',
-  '\'',
-  '`',
-  '"',
+  const lines = [];
+  let line = [];
+  for (let i = 0; i < elems.length; i++) {
+    if (elems[i] === '\n') {
+      lines.push(line);
+      line = [];
+      continue;
+    }
+    line.push(elems[i]);
+  }
 
-  // non visibles
-  ' ',
-  '\n',
-  '\r'
-];
+  return lines;
+}
 
 const invisible = [
   ' ',
@@ -168,44 +183,29 @@ const blank_vars = [
   'let',
 ];
 
-interface RawToken {
-  type: 'raw';
-  value: string;
+function tokenize_raw(code: string) : string[] {
+  const tokens: string[] = [];
+  const matches = code.matchAll(/\W/g);
 
-  pos: number;
+  let current_token = 0;
+  for (const match of matches) {
+    const i = match.index;
+
+    if (current_token != i) tokens.push(code.slice(current_token, i));
+    tokens.push(match[0]);
+    current_token = i + 1;
+  }
+
+  if (current_token < code.length) tokens.push(code.slice(current_token))
+  return tokens;
 }
 
 export function tokenize(code: string) {
-  const tokens: RawToken[] = [];
-  let current_token: RawToken = {
-    type: 'raw',
-    value: '',
+  console.time('raw_tokenize');
+  const tokens = tokenize_raw(code);
+  console.timeEnd('raw_tokenize');
 
-    pos: 0,
-  };
-
-  for (let i = 0; i < code.length; i++) {
-    const char = code[i];
-    if (breaking_chars.includes(char)) {
-      if (current_token.value) tokens.push(current_token);
-      tokens.push({
-        type: 'raw',
-        value: char,
-
-        pos: i,
-      });
-      current_token = {
-        type: 'raw',
-        value: '',
-    
-        pos: i + 1,
-      };
-    } else {
-      current_token.value += char;
-    }
-  }
-  if (current_token) tokens.push(current_token);
-
+  console.time('2_tokenize');
   const new_tokens: Token[] = [];
   const env = {
     comment: null,
@@ -213,161 +213,180 @@ export function tokenize(code: string) {
     brackets: [],
   };
 
-  for (let i = 0; i < tokens.length; i++) {
-    const token = tokens[i];
-    if (env.string) {
-      const length = token.pos - env.string.pos;
-      const previous_token = tokens[i - 1];
-      if (previous_token.value != '\\' && token.value == env.string.value) {
-        const value = code.substr(env.string.pos, length + 1);
-        new_tokens.push(STRING(value));
+  let pos = 0;
 
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i - 1]) pos += tokens[i - 1].length;
+    const previous_token = tokens[i - 1];
+    const token = tokens[i];
+
+    if (env.string) {
+      if (token === '\n' && env.string.value !== '`') {
+        new_tokens.push(STRING(code.slice(env.string.pos, pos), env.string.pos));
+        new_tokens.push('\n');
         env.string = null;
+        continue;
+      }
+      if (previous_token !== '\\' && token === env.string.value) {
+        new_tokens.push(STRING(code.slice(env.string.pos, pos + 1), env.string.pos));
+        env.string = null;
+        continue;
+      }
+      
+      continue;
+    }
+    if (env.comment) {
+      const length = pos - env.comment.pos;
+      const next_token = tokens[i + 1] ?? '';
+      const value = token + next_token;
+
+      if (env.comment.value === '//' && token === '\n') {
+        const value = code.substr(env.comment.pos, length);
+        new_tokens.push(COMMENT(value, env.comment.pos));
+        
+        i--;
+        pos--;
+        env.comment = null;
+        continue;
+      }
+      if (env.comment.value === '/*' && value === '*/') {
+        const value = code.substr(env.comment.pos, length + 2);
+        new_tokens.push(COMMENT(value, env.comment.pos));
+        
+        i++;
+        pos++;
+        env.comment = null;
         continue;
       }
 
       continue;
     }
-    if (env.comment) {
-      const length = token.pos - env.comment.pos;
-      const next_token = tokens[i + 1]?.value ?? '';
-
-      const value = token.value == '\n' ? '\n' : `${token.value}${next_token}`;
-
-      const comment_end = (env.comment.value == '//' && value == '\n') || (env.comment.value == '/*' && value == '*/');
-      if (comment_end) {
-        const value = code.substr(env.comment.pos, length + (token.value == '\n' ? 0 : 2));
-        new_tokens.push(COMMENT(value));
-        if (token.value == '\n') i--;
-        if (token.value != '\n') i++;
-        env.comment = null;
-      }
-      continue;
-    }
-    if (token.value == '/') {
-      const next_token = tokens[i + 1];
-      if (next_token && (next_token.value == '/' || next_token.value == '*')) {
+    if (token === '/') {
+      const next_token = tokens[i + 1] ?? '';
+      if (next_token === '/' || next_token === '*') {
         env.comment = {
-          value: token.value + next_token.value,
-          pos: token.pos,
+          value: token + next_token,
+          pos,
         };
         continue;
       }
     }
 
-    if (env.brackets.length > 0 && token.value == env.brackets[env.brackets.length - 1].closing) {
+    if (env.brackets.length > 0 && token === env.brackets[env.brackets.length - 1].closing) {
       const pos = env.brackets.pop().pos;
       const value = new_tokens.splice(pos, new_tokens.length - pos);
 
-      if (token.value == ')') new_tokens.push(PARENTHESES(value));
-      if (token.value == ']') new_tokens.push(BRACES(value));
-      if (token.value == '}') new_tokens.push(BRACKETS(value));
+      if (token === ')') new_tokens.push(PARENTHESES(value, pos));
+      if (token === ']') new_tokens.push(BRACES(value, pos));
+      if (token === '}') new_tokens.push(BRACKETS(value, pos));
 
       continue;
     }
 
-    if (brackets.includes(token.value)) {
+    if (brackets.includes(token)) {
       let closing = '';
-      if (token.value == '(') closing = ')';
-      if (token.value == '[') closing = ']';
-      if (token.value == '{') closing = '}';
+      if (token === '(') closing = ')';
+      if (token === '[') closing = ']';
+      if (token === '{') closing = '}';
 
       env.brackets.push({
-        value: token.value,
+        value: token,
         pos: new_tokens.length,
         closing,
       });
       continue;
     }
 
-    if (strings.includes(token.value)) {
+    if (strings.includes(token)) {
       const previous_token = tokens[i - 1];
-      if (!previous_token || previous_token.value != '\\') {
+      if (!previous_token || previous_token !== '\\') {
         env.string = {
-          value: token.value,
-          pos: token.pos,
+          value: token,
+          pos,
         };
         continue;
       }
     }
 
-    if (blank_vars.includes(token.value)) {
-      new_tokens.push(KEYWORD(token.value));
-      const between: string[] = [];
+    if (blank_vars.includes(token)) {
+      new_tokens.push(KEYWORD(token, pos));
 
-      while (tokens[i + 1] && invisible.includes(tokens[i + 1].value)) {
-        between.push(tokens[i + 1].value);
+      let between = "";
+      while (invisible.includes(tokens[i + 1])) {
+        pos += tokens[i].length;
         i++;
+        between += tokens[i];
       }
       
       new_tokens.push(...between);
-      if (!/\W/g.test(tokens[i + 1].value)) {
-        new_tokens.push(tokens[i + 1].value);
+      if (!/\W/g.test(tokens[i + 1])) {
+        new_tokens.push(tokens[i + 1]);
+        pos += tokens[i].length;
         i++;
       }
       continue;
     }
 
-    const keyword = keywords.map(v => {
-      let total = '';
-      let j = 0;
-      while (v.startsWith(total) && total.length <= v.length && tokens.length > i + j) {
-        total += tokens[i + j].value;
-        if (total == v) return [v, j] as [string, number];
-        j++;
+    if (keywords.includes(token)) {
+      new_tokens.push(KEYWORD(token, pos));
+      continue;
+    }
+
+    if (token == 'true' || token == 'false') {
+      new_tokens.push(BOOLEAN(token, pos));
+      continue;
+    }
+
+    if (statics.includes(token)) {
+      new_tokens.push(STATIC(token, pos));
+      continue;
+    }
+
+    if (!/\D/g.test(token)) {
+      new_tokens.push(NUMBER(token, pos));
+      continue;
+    }
+
+    let operator = '';
+    for (let j = 0; j < operators.length; j++) {
+      if (operators[j].length <= operator.length) continue;
+
+      const t = code.substr(pos, operators[j].length);
+      if (t === operators[j]) operator = operators[j];
+    }
+
+    if (operator) {
+      let total = token.length;
+      while (total < operator.length) {
+        i++;
+        total += tokens[i].length;
       }
-      return null;
-    }).filter(v => v).sort((a, b) => b[0].length - a[0].length)[0];
-    const operator = operators.map(v => {
-      let total = '';
-      let j = 0;
-      while (v.startsWith(total) && total.length <= v.length && tokens.length > i + j) {
-        total += tokens[i + j].value;
-        if (total == v) return [v, j] as [string, number];
-        j++;
-      }
-      return null;
-    }).filter(v => v).sort((a, b) => b[0].length - a[0].length)[0];
 
-    if (operator && operator[0].length > (keyword?.[0].length || 0)) {
-      new_tokens.push(OPERATOR(operator[0]));
-      i += operator[1];
-      continue;
-    } else if (keyword) {
-      new_tokens.push(KEYWORD(keyword[0]));
-      i += keyword[1];
+      pos += total;
+      pos -= tokens[i].length;
+
+      new_tokens.push(OPERATOR(operator, pos));
       continue;
     }
 
-    if (token.value == 'true' || token.value == 'false') {
-      new_tokens.push(BOOLEAN(token.value));
+    if (!/\W/g.test(token)) {
+      new_tokens.push(VARIABLE(token, pos));
       continue;
     }
 
-    if (!/\D/g.test(token.value)) {
-      new_tokens.push(NUMBER(token.value));
-      continue;
-    }
-    
-    if (statics.includes(token.value)) {
-      new_tokens.push(STATIC(token.value));
-      continue;
-    }
-
-    if (!/\W/g.test(token.value)) {
-      new_tokens.push(VARIABLE(token.value));
-      continue;
-    }
-
-    if (token.value == '.') {
+    if (token == '.') {
       const next_token = tokens[i + 1];
-      console.log(next_token);
-      if (next_token && !/\D/g.test(next_token.value)) new_tokens.push(NUMBER(token.value)); else new_tokens.push(KEYWORD(token.value));
+      if (next_token && !/\D/g.test(next_token)) new_tokens.push(NUMBER(token, pos)); else new_tokens.push(token);
       continue;
     }
 
-    new_tokens.push(token.value);
+    new_tokens.push(token);
   }
+  console.timeEnd('2_tokenize');
 
   return new_tokens;
+}
+
+function getTokenFromPos(tokens: Token[], pos: number) {
+
 }
