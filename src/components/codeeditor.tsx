03a2d21236f61d6362d62f5409e54abed8cf1f94
firecraftgaming <eliyah@sundstroem.com>
2021-08-01 21:52:42 +0200

    feat: added example

--- src/components/codeeditor.tsx
+++ src/components/codeeditor.tsx
@@ -1,91 +1,44 @@
-// SOURCE: https://gist.github.com/cenguidanos/58efcf54c5539101d9a47345d6cea35d
-
-import React, { useRef, useEffect, useState } from "react";
-
-import { EditorView, keymap, ViewUpdate } from "@codemirror/view";
-import { autocompletion } from "@codemirror/autocomplete";
-import { lineNumbers } from "@codemirror/gutter";
-import { EditorState, Compartment } from "@codemirror/state";
-import { basicSetup } from "@codemirror/basic-setup";
-import { defaultTabBinding } from "@codemirror/commands";
-
-// Themes
-import { oneDark } from "@codemirror/theme-one-dark";
-
-// Languages
-import { htmlLanguage, html } from "@codemirror/lang-html";
-import { language } from "@codemirror/language";
-import { javascript } from "@codemirror/lang-javascript";
+import React from "react";
+import { useEffect } from "react";
+import { Editor } from "skyline-editor"
+import WebFont from 'webfontloader';
 
 const CodeEditor: React.FC<{ initialValue: string }> = ({ initialValue }) => {
-  // Local state
-  const [editorValue, setEditorValue] = useState("");
-  const [editorTreeValue, setEditorTreeValue] = useState([]);
+  const [editor, setEditor] = React.useState<Editor>(null);
+  const canvas = React.useRef(null);
 
-  // Ref of the editor
-  const editor = useRef<EditorView>();
-
-  // Event listener on editor updates
-  const onUpdate = () =>
-    EditorView.updateListener.of((v: ViewUpdate) => {
-      const doc = v.state.doc;
-
-      const value = doc.toString();
-
-      if (value !== editorValue) setEditorValue(value);
-      let treeArray = [];
-      treeArray = [...doc.toJSON()];
-
-      if (treeArray !== editorTreeValue) setEditorTreeValue(treeArray);
+  useEffect(() => {
+    WebFont.load({
+      custom: {
+        families: ['consolas'],
+        urls: ['/src/consolas.woff']
+      }
     });
 
-  // Initilize view
-  useEffect(function initEditorView() {
-    const el = document.getElementById("codemirror-editor-wrapper");
-
-    const tabSize = new Compartment(),
-      languageConf = new Compartment();
-
-    const autoLanguage = EditorState.transactionExtender.of((tr) => {
-      if (!tr.docChanged) return null;
-      let docIsHTML = /^\s*</.test(tr.newDoc.sliceString(0, 100));
-      let stateIsHTML = tr.startState.facet(language) === htmlLanguage;
-      if (docIsHTML === stateIsHTML) return null;
-      return {
-        effects: languageConf.reconfigure(docIsHTML ? html() : javascript()),
-      };
-    });
-
-    editor.current = new EditorView({
-      state: EditorState.create({
-        doc: initialValue,
-        extensions: [
-          basicSetup,
-          autocompletion(),
-          lineNumbers(),
-          keymap.of([defaultTabBinding]),
-          tabSize.of(EditorState.tabSize.of(4)),
-          languageConf.of(javascript()),
-          autoLanguage,
-          oneDark,
-          onUpdate(),
-        ],
-      }),
-      parent: el as Element,
-    });
-    // eslint-disable-next-line
+    const editor = new Editor(initialValue);
+    setEditor(editor);
   }, []);
 
+  useEffect(() => {
+    if (!editor) return;
+    if (!canvas) return;
+
+    editor.mount(canvas.current);
+  }, [editor, canvas]);
+
+  console.log(editor);
+
   return (
-    <div
-      id="codemirror-editor-wrapper"
+    <canvas
+      id="skyline-editor-wrapper"
       style={{
-        position: "absolute",
-        left: "0",
         width: "100%",
         height: "100%",
         background: "#1c1e26",
+
+        cursor: 'text'
       }}
+      ref={canvas}
     />
   );
 };
