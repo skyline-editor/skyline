63afdba8204ef268cf8965e1219d845c78d2d5f3
Tejas Ravishankar <xtremedevx@gmail.com>
2021-08-02 20:10:28 +0400

    feat: format files

--- src/components/codeeditor.tsx
+++ src/components/codeeditor.tsx
@@ -1,50 +1,51 @@
-import React from "react";
-import { useEffect } from "react";
-import { Editor } from "../../editor/"
-import WebFont from 'webfontloader';
+import React from 'react'
+import { useEffect } from 'react'
+import { Editor } from '../../editor/'
+import WebFont from 'webfontloader'
 
 const CodeEditor: React.FC<{ initialValue: string }> = ({ initialValue }) => {
-  const [editor, setEditor] = React.useState<Editor>(null);
-  const canvas = React.useRef(null);
+  const [editor, setEditor] = React.useState<Editor>(null)
+  const canvas = React.useRef(null)
 
   useEffect(() => {
     WebFont.load({
       custom: {
         families: ['consolas'],
-        urls: ['/src/consolas.woff']
-      }
-    });
+        urls: ['/src/consolas.woff'],
+      },
+    })
 
-    const editor = new Editor(initialValue);
+    const editor = new Editor(initialValue)
+
     editor.on('save', (_editor) => {
-      console.log(editor.code);
-    });
-    
-    setEditor(editor);
-  }, []);
+      console.log(editor.code)
+    })
+
+    setEditor(editor)
+  }, [])
 
   useEffect(() => {
-    if (!editor) return;
-    if (!canvas) return;
+    if (!editor) return
+    if (!canvas) return
 
-    editor.mount(canvas.current);
-  }, [editor, canvas]);
+    editor.mount(canvas.current)
+  }, [editor, canvas])
 
-  console.log(editor);
+  console.log(editor)
 
   return (
     <canvas
       id="skyline-editor-wrapper"
       style={{
-        width: "100%",
-        height: "100%",
-        background: "#1c1e26",
+        width: '100%',
+        height: '100%',
+        background: '#1c1e26',
 
-        cursor: 'text'
+        cursor: 'text',
       }}
       ref={canvas}
     />
-  );
-};
+  )
+}
 
-export default CodeEditor;
+export default CodeEditor
