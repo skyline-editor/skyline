edba8f1589bdefb3aa6ffcc054f0d7d817d3c4e6
Tejas Ravishankar <xtremedevx@gmail.com>
2021-08-03 14:25:51 +0400

    feat: apply license to files

--- src/components/codeeditor.tsx
+++ src/components/codeeditor.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { Editor } from '../../editor/'
-import WebFont from 'webfontloader'
+/*
+    Copyright 2021 Tejas Ravishankar, Eliyah Sundström, Pranav Doshi
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+import React from 'react';
+import { useEffect } from 'react';
+import { Editor } from '../../editor/';
+import WebFont from 'webfontloader';
 import typescript from '@skyline-editor/language-typescript';
 import FileSystem from '../model/FileSystem';
 
@@ -19,7 +32,7 @@ const CodeEditor: React.FC<{ initialValue: string }> = ({ initialValue }) => {
 
     const fileSystem = new FileSystem();
 
-    const editor = new Editor(initialValue)
+    const editor = new Editor(initialValue);
     editor.language = typescript;
 
     editor.on('save', (editor) => {
--- src/model/FileSystem.ts
+++ src/model/FileSystem.ts
@@ -1,3 +1,16 @@
+/*
+    Copyright 2021 Tejas Ravishankar, Eliyah Sundström, Pranav Doshi
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 // TODO: implement FileSystem
 export default class FileSystem {
   async listFiles(path: string): Promise<string[]> {
@@ -8,15 +21,9 @@ export default class FileSystem {
     return '';
   }
 
-  async renameFile(path: string, new_path: string): Promise<void> {
-    
-  }
+  async renameFile(path: string, new_path: string): Promise<void> {}
 
-  async removeFile(path: string): Promise<void> {
-    
-  }
+  async removeFile(path: string): Promise<void> {}
 
-  async writeFile(path: string, content: string): Promise<void> {
-    
-  }
+  async writeFile(path: string, content: string): Promise<void> {}
 }
