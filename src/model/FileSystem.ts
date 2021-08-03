a0cb9f03ef1a531d7962fb61bd9f2aca5bd6e7d7
firecraftgaming <eliyah@sundstroem.com>
2021-08-03 10:56:43 +0200

    chore: edited filesystem for web

--- src/model/FileSystem.ts
+++ src/model/FileSystem.ts
@@ -1,43 +1,22 @@
-import { fs } from '@tauri-apps/api';
-
+// TODO: implement FileSystem
 export default class FileSystem {
-  listFiles(path: string): Promise<string[]> {
-    return fs.readDir(path).then((files) => {
-      const paths = new Array<string>(files.length);
-      for (let i = 0; i < files.length; i++) {
-        paths[i] = files[i].path;
-      }
-
-      return paths;
-    });
+  async listFiles(path: string): Promise<string[]> {
+    return [];
   }
 
-  readTextFile(path: string): Promise<string> {
-    return fs.readTextFile(path).then((text) => text ?? '');
+  async readTextFile(path: string): Promise<string> {
+    return '';
   }
 
-  renameFile(path: string, new_path: string): Promise<void> {
-    return fs
-      .renameFile(path, new_path)
-      .then((_) => null)
-      .catch((err) => err);
+  async renameFile(path: string, new_path: string): Promise<void> {
+    
   }
 
-  removeFile(path: string): Promise<void> {
-    return fs
-      .removeFile(path)
-      .then((_) => null)
-      .catch((err) => err);
+  async removeFile(path: string): Promise<void> {
+    
   }
 
-  writeFile(path: string, content: string): Promise<void> {
-    // todo, only append extra changes to the file
-    return fs
-      .writeFile({
-        path: path,
-        contents: content,
-      })
-      .then((_) => null)
-      .catch((err) => err);
+  async writeFile(path: string, content: string): Promise<void> {
+    
   }
 }
