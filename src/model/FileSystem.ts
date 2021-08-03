34dd70bff8a15a47d7ce98c3bff4a5a73da7685c
firecraftgaming <eliyah@sundstroem.com>
2021-08-03 10:58:41 +0200

    chore: made filesystem use async await

--- src/model/FileSystem.ts
+++ src/model/FileSystem.ts
@@ -1,38 +1,38 @@
 import { fs } from '@tauri-apps/api';
 
 export default class FileSystem {
-  listFiles(path: string): Promise<string[]> {
-    return fs.readDir(path).then((files) => {
+  async listFiles(path: string): Promise<string[]> {
+    return await fs.readDir(path).then((files) => {
       const paths = new Array<string>(files.length);
       for (let i = 0; i < files.length; i++) {
         paths[i] = files[i].path;
       }
 
       return paths;
-    });
+    }).catch(err => err);
   }
 
-  readTextFile(path: string): Promise<string> {
-    return fs.readTextFile(path).then((text) => text ?? '');
+  async readTextFile(path: string): Promise<string> {
+    return await fs.readTextFile(path).then((text) => text ?? '').catch(err => err);
   }
 
-  renameFile(path: string, new_path: string): Promise<void> {
-    return fs
+  async renameFile(path: string, new_path: string): Promise<void> {
+    return await fs
       .renameFile(path, new_path)
       .then((_) => null)
       .catch((err) => err);
   }
 
-  removeFile(path: string): Promise<void> {
-    return fs
+  async removeFile(path: string): Promise<void> {
+    return await fs
       .removeFile(path)
       .then((_) => null)
       .catch((err) => err);
   }
 
-  writeFile(path: string, content: string): Promise<void> {
+  async writeFile(path: string, content: string): Promise<void> {
     // todo, only append extra changes to the file
-    return fs
+    return await fs
       .writeFile({
         path: path,
         contents: content,
