ced7e74c77ef20e0197d4651ec0592e3428cd113
Pranav Doshi <pranav.rdoshi@gmail.com>
2021-08-04 20:14:45 +0530

    feat: created title bar

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,25 @@ function App() {
       <header>
         <title>Skyline Editor</title>
       </header>
+
+      <div data-tauri-drag-region className="titlebar">
+        <div className="titlebar-button" id="titlebar-minimize">
+          <img
+            src="https://api.iconify.design/mdi:window-minimize.svg"
+            alt="minimize"
+          />
+        </div>
+        <div className="titlebar-button" id="titlebar-maximize">
+          <img
+            src="https://api.iconify.design/mdi:window-maximize.svg"
+            alt="maximize"
+          />
+        </div>
+        <div className="titlebar-button" id="titlebar-close">
+          <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+        </div>
+      </div>
+
       {/* Code Editor */}
       <div className="editor">
         <Sidebar />
